feee7d88082159492478a04f3d8eade31bb2823a
Tim <tim.zaugg93@gmail.com>
2022-05-19 14:48:57 +0200

    bodysounds and wave

--- p5/Soundsketch2/sketch.js
+++ p5/Soundsketch2/sketch.js
@@ -9,6 +9,33 @@ let soundFiles = [
   "sounds/Miles_Davis_Bird_of_Paradise.mp3",
   "sounds/Passenger_Bird_in_Flight.mp3",
   "sounds/Eminem_Crack_A_Bottle.mp3",
+  "sounds/hand.mp3",
+  "sounds/Bier-ganz-Balz.mp3",
+  "sounds/Bier-ganz-Tim.mp3",
+  "sounds/Chips-kauen.mp3",
+  "sounds/Energy-Drink-schlucken.mp3",
+  "sounds/Goerps1.mp3",
+  "sounds/Goerps2.mp3",
+  "sounds/Goerps3.mp3",
+  "sounds/Goerps4.mp3",
+  "sounds/Goerps5.mp3",
+  "sounds/Goerps6.mp3",
+  "sounds/Gurke-kauen.mp3",
+  "sounds/Knack_1.mp3",
+  "sounds/Knacks_2.mp3",
+  "sounds/Knacks_3.mp3",
+  "sounds/Knacks_4.mp3",
+  "sounds/Knacks_5.mp3",
+  "sounds/Knacks_6.mp3",
+  "sounds/Knacks_7.mp3",
+  "sounds/Knacks_8.mp3",
+  "sounds/Knacks_9.mp3",
+  "sounds/Knacks_10.mp3",
+  "sounds/Salat-kauen.mp3",
+  "sounds/Snackruebli-kauen.mp3",
+  "sounds/tief-gsi-du.mp3",
+  "sounds/Wasser-schlucken-Balz.mp3",
+  "sounds/Wasser-schlucken-Tim.mp3",
 ];
 let songs = [];
 
@@ -20,15 +47,15 @@ function preload() {
 
 function setup() {
   //createCanvas(windowWidth, windowHeight);
-  createCanvas(windowWidth, windowHeight);
+
   //background("blue");
   rectMode(CENTER);
 
   /// raster buttons mit verschiedenen songs ///
   for (let i = 0; i < soundFiles.length; i++) {
     let left = 100;
-    let top = 100;
-    let xAbstand = 100;
+    let top = 90;
+    let xAbstand = 90;
     let moduloCoeff = 4;
     let x = i % moduloCoeff; // 0,1,2,0,1,2,0,1,2  -> X koordinate
 
@@ -39,17 +66,63 @@ function setup() {
     buttons.push(new Button(left + x * xAbstand, y, songs[i]));
   }
 
-  sliderVolume = createSlider(0, 1, 0.5, 0.02);
+  sliderVolume = createSlider(0, 1, 0.5, 0.01);
   sliderVolume.addClass("mySliders");
+  amp = new p5.Amplitude();
+  fft = new p5.FFT();
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight - 80);
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].display();
   }
-  ////---Volume of all Songs in the sketch---////
-  sliderVolume.position(width / 2 - sliderVolume.width / 2, height / 2);
   outputVolume(sliderVolume.value());
+  ////---Volume of all Songs in the sketch---////
+  sliderVolume.position(
+    width / 2 - sliderVolume.width / 2,
+    height - height / 10
+  );
+
+  //text("🔊", width / 2, height - 100);
+  let vol = amp.getLevel();
+  let diam = map(vol, 0, 0.5, 50, 200);
+  strokeWeight(5);
+
+  let barHeight = height - height / 6;
+  let barAbstand = 30;
+
+  line(width, barHeight - barAbstand, diam / 1.5, barHeight - barAbstand);
+  line(0, barHeight, diam, barHeight);
+  line(0, barHeight + barAbstand, diam / 2, barHeight + barAbstand);
+
+  line(width, barHeight - 2 * barAbstand);
+
+  ellipse(width / 2, height / 2, diam, diam);
+
+  //console.log(vol);
+
+  let spectrum = fft.analyze();
+  //console.log(spectrum);
+  noStroke();
+  fill(207, 137, 85);
+  for (let i = 0; i < spectrum.length; i++) {
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h);
+  }
+
+  let waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(20);
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map(waveform[i], -1, 1, 0, height);
+    vertex(x, y);
+    //console.log(x);
+  }
+  endShape();
 }
 
 function mousePressed() {
@@ -98,7 +171,7 @@ class Button {
 
   switch() {
     if (!this.isPressed) {
-      this.song.play();
+      this.song.loop();
       this.isPressed = true;
     } else {
       this.song.stop();
